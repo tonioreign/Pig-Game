3410d66818eed2814c1289dddfcdbe1f291ec19f
tonioreign <tonioreign@gmail.com>
2022-04-21 21:35:45 -0700

    Hold Functions Added

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 
 //Selecting elements
 const btnRoll = document.querySelector('.btn--roll');
-const holdEl = document.querySelector('.btn--hold');
+const btnHold = document.querySelector('.btn--hold');
 const btnNew = document.querySelector('.btn--new');
 const diceEl = document.querySelector('.dice');
+const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
-const score2El = document.querySelector('#score--2');
+const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
-const current2El = document.querySelector('#current--2');
+const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-const player2El = document.querySelector('.player--2');
 const playerActive = document.querySelector('.player--active');
 
 
@@ -24,8 +24,17 @@ diceEl.classList.add('hidden');
 //Identifying variables
 const scores = [0, 0];
 let currentScore = 0;
-let activePlayer = 1;
+let activePlayer = 0;
 
+//Functions
+
+const switchPlayers = () => {
+    document.getElementById(`current--${activePlayer}`).textContent = 0; 
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player1El.classList.toggle('player--active'); //adds or removes the class selected
+    player0El.classList.toggle('player--active');
+}
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', e => {
@@ -37,11 +46,18 @@ btnRoll.addEventListener('click', e => {
         currentScore += dice;
         document.getElementById(`current--${activePlayer}`).textContent = currentScore; 
     }
-    else if (dice === 1){
-        player2El.classList.add('player--active');
-        player1El.classList.remove('player--active');
-    }
     else {
+        switchPlayers();
+    }
+})
+
+//Hold functionality
+btnHold.addEventListener('click', e => {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    switchPlayers();
+
+    if (scores[activePlayer] == 100) {
 
     }
 })
